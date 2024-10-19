2b06408e96d9d2683ff75981028884cda4f42ca8
mohammed893 <pspcpspc6@gmail.com>
2024-10-19 19:17:12 +0300

    [edit] Creating DB

--- database/physicaltherabydb.sql
+++ database/physicaltherabydb.sql
@@ -1,6 +1,10 @@
---
 -- PostgreSQL database dump
---
+
+-- Create a database if it doesn't exist
+CREATE DATABASE physicaltheraby;
+
+-- Connect to the created database
+\c physicaltheraby;
 
 -- Dumped from database version 16.4
 -- Dumped by pg_dump version 16.0
@@ -16,23 +20,15 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
---
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 
-
---
 -- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
---
 
 COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
-
---
 -- Name: doctors_doctor_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.doctors_doctor_id_seq
     AS integer
@@ -42,16 +38,13 @@ CREATE SEQUENCE public.doctors_doctor_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 ALTER SEQUENCE public.doctors_doctor_id_seq OWNER TO postgres;
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
 -- Name: doctors; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.doctors (
     full_name text NOT NULL,
@@ -67,12 +60,9 @@ CREATE TABLE public.doctors (
     doctor_id integer DEFAULT nextval('public.doctors_doctor_id_seq'::regclass) NOT NULL
 );
 
-
 ALTER TABLE public.doctors OWNER TO postgres;
 
---
 -- Name: patients_patient_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.patients_patient_id_seq
     AS integer
@@ -82,12 +72,9 @@ CREATE SEQUENCE public.patients_patient_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 ALTER SEQUENCE public.patients_patient_id_seq OWNER TO postgres;
 
---
 -- Name: patients; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.patients (
     patient_id integer DEFAULT nextval('public.patients_patient_id_seq'::regclass) NOT NULL,
@@ -100,60 +87,33 @@ CREATE TABLE public.patients (
     follow_up boolean NOT NULL
 );
 
-
 ALTER TABLE public.patients OWNER TO postgres;
 
---
 -- Data for Name: doctors; Type: TABLE DATA; Schema: public; Owner: postgres
---
 
-
-
---
 -- Data for Name: patients; Type: TABLE DATA; Schema: public; Owner: postgres
---
 
-
-
---
 -- Name: doctors_doctor_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
 
 SELECT pg_catalog.setval('public.doctors_doctor_id_seq', 1, false);
 
-
---
 -- Name: patients_patient_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
 
 SELECT pg_catalog.setval('public.patients_patient_id_seq', 1, false);
 
-
---
 -- Name: doctors doctors_doctor_id_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.doctors
     ADD CONSTRAINT doctors_doctor_id_pkey PRIMARY KEY (doctor_id);
 
-
---
 -- Name: doctors doctors_national_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.doctors
     ADD CONSTRAINT doctors_national_id_key UNIQUE (national_id);
 
-
---
 -- Name: patients patients_patien_id_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.patients
     ADD CONSTRAINT patients_patien_id_pkey PRIMARY KEY (patient_id);
 
-
---
 -- PostgreSQL database dump complete
---
-
