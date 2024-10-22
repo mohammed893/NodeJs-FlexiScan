94d2d7219cf750775d1e79b52b5fc2ef1dd28340
mohammed893 <pspcpspc6@gmail.com>
2024-10-22 23:21:46 +0300

    Auth-test

---
+++ routes/Doctor/Doctors.controller.js
@@ -0,0 +1,20 @@
+const {pool} = require('../../models/configrations');
+
+const getDoctor = async(req , res) => {
+    doctor_id = req.user.user_id;
+    console.log(doctor_id);
+    try{
+        const result = await pool.query('SELECT * FROM doctors WHERE doctor_id = $1', [doctor_id]);
+        if(result.rows.length === 0){
+            return res.status(404).json("Doctor Not found !");
+        }
+        res.status(200).json(result.rows[0]);
+    }catch(err){
+        console.log("error getting a doctor : " , err)
+        res.status(500).json({error : "Server Error while finding a doctor !"});
+    }
+}
+
+module.exports = {
+    getDoctor
+}
